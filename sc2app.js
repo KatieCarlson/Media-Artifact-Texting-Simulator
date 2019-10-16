9d80a3270c6a9540ecde3e566013cc4c0974fbed
KatieCarlson <54963526+KatieCarlson@users.noreply.github.com>
2019-10-16 18:59:40 -0400

    Uploaded HTML CSS JS and Source FIles

---
+++ sc2app.js
@@ -0,0 +1,159 @@
+var text1 = document.getElementById("text2-1");
+
+text1.addEventListener("click", function(){
+  
+  // X: horizontal distance from top left
+  // Y: vertical distance from top left
+  var ONEcoords = []; 
+  
+  var ZEROcoords = [];
+  
+  var X, Y;
+  var rand; 
+  var op = document.getElementById("oneParent").childNodes;
+  var zp = document.getElementById("zeroParent").childNodes;
+  
+  for (var i = 2; i < 302; i++) {
+    
+    rand = Math.floor(Math.random() * ONEcoords.length / 2) * 2;
+    X = ONEcoords[rand] + 20 - 210;
+    Y = ONEcoords[rand + 1] + 35 - 230;
+    ONEcoords.splice(rand, 2);
+    op[i].style.transform = 'translate('+X+'px'+', '+Y+'px)';
+    
+    rand = Math.floor(Math.random() * ZEROcoords.length / 2) * 2;
+    X = ZEROcoords[rand] - 270;
+    Y = ZEROcoords[rand + 1] + 35 - 230;
+    ZEROcoords.splice(rand, 2);
+    zp[i].style.transform = 'translate('+X+'px'+', '+Y+'px)';
+  }
+
+});
+
+var text2 = document.getElementById("text2-2");
+
+text2.addEventListener("click", function(){
+  
+  // X: horizontal distance from top left
+  // Y: vertical distance from top left
+  var ONEcoords = []; 
+  
+  var ZEROcoords = [];
+  
+  var X, Y;
+  var rand; 
+  var op = document.getElementById("oneParent").childNodes;
+  var zp = document.getElementById("zeroParent").childNodes;
+  
+  for (var i = 2; i < 302; i++) {
+    
+    rand = Math.floor(Math.random() * ONEcoords.length / 2) * 2;
+    X = ONEcoords[rand] + 20 - 210;
+    Y = ONEcoords[rand + 1] + 35 - 230;
+    ONEcoords.splice(rand, 2);
+    op[i].style.transform = 'translate('+X+'px'+', '+Y+'px)';
+    
+    rand = Math.floor(Math.random() * ZEROcoords.length / 2) * 2;
+    X = ZEROcoords[rand] - 270;
+    Y = ZEROcoords[rand + 1] + 35 - 230;
+    ZEROcoords.splice(rand, 2);
+    zp[i].style.transform = 'translate('+X+'px'+', '+Y+'px)';
+  }
+
+});
+
+var text3 = document.getElementById("text2-3");
+
+text3.addEventListener("click", function(){
+  
+  // X: horizontal distance from top left
+  // Y: vertical distance from top left
+  var ONEcoords = []; 
+  
+  var ZEROcoords = [];
+  
+  var X, Y;
+  var rand; 
+  var op = document.getElementById("oneParent").childNodes;
+  var zp = document.getElementById("zeroParent").childNodes;
+  
+  for (var i = 2; i < 302; i++) {
+    
+    rand = Math.floor(Math.random() * ONEcoords.length / 2) * 2;
+    X = ONEcoords[rand] + 20 - 210;
+    Y = ONEcoords[rand + 1] + 35 - 230;
+    ONEcoords.splice(rand, 2);
+    op[i].style.transform = 'translate('+X+'px'+', '+Y+'px)';
+    
+    rand = Math.floor(Math.random() * ZEROcoords.length / 2) * 2;
+    X = ZEROcoords[rand] - 270;
+    Y = ZEROcoords[rand + 1] + 35 - 230;
+    ZEROcoords.splice(rand, 2);
+    zp[i].style.transform = 'translate('+X+'px'+', '+Y+'px)';
+  }
+
+});
+
+var text4 = document.getElementById("text2-4");
+
+text4.addEventListener("click", function(){
+  
+  // X: horizontal distance from top left
+  // Y: vertical distance from top left
+  var ONEcoords = []; 
+  
+  var ZEROcoords = [];
+  
+  var X, Y;
+  var rand; 
+  var op = document.getElementById("oneParent").childNodes;
+  var zp = document.getElementById("zeroParent").childNodes;
+  
+  for (var i = 2; i < 302; i++) {
+    
+    rand = Math.floor(Math.random() * ONEcoords.length / 2) * 2;
+    X = ONEcoords[rand] + 20 - 210;
+    Y = ONEcoords[rand + 1] + 35 - 230;
+    ONEcoords.splice(rand, 2);
+    op[i].style.transform = 'translate('+X+'px'+', '+Y+'px)';
+    
+    rand = Math.floor(Math.random() * ZEROcoords.length / 2) * 2;
+    X = ZEROcoords[rand] - 270;
+    Y = ZEROcoords[rand + 1] + 35 - 230;
+    ZEROcoords.splice(rand, 2);
+    zp[i].style.transform = 'translate('+X+'px'+', '+Y+'px)';
+  }
+
+});
+
+var text5 = document.getElementById("text2-5");
+
+text5.addEventListener("click", function(){
+  
+  // X: horizontal distance from top left
+  // Y: vertical distance from top left
+  var ONEcoords = []; 
+  
+  var ZEROcoords = [];
+  
+  var X, Y;
+  var rand; 
+  var op = document.getElementById("oneParent").childNodes;
+  var zp = document.getElementById("zeroParent").childNodes;
+  
+  for (var i = 2; i < 302; i++) {
+    
+    rand = Math.floor(Math.random() * ONEcoords.length / 2) * 2;
+    X = ONEcoords[rand] + 20 - 210;
+    Y = ONEcoords[rand + 1] + 35 - 230;
+    ONEcoords.splice(rand, 2);
+    op[i].style.transform = 'translate('+X+'px'+', '+Y+'px)';
+    
+    rand = Math.floor(Math.random() * ZEROcoords.length / 2) * 2;
+    X = ZEROcoords[rand] - 270;
+    Y = ZEROcoords[rand + 1] + 35 - 230;
+    ZEROcoords.splice(rand, 2);
+    zp[i].style.transform = 'translate('+X+'px'+', '+Y+'px)';
+  }
+
+});
